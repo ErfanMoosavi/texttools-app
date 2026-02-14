7d73f83b1beb0d13a874799440d71a95f1a881b4
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-14 16:22:19 +0330

    Move routes from main.py to routes.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from fastapi import FastAPI, status, HTTPException
-from openai import OpenAI
-from texttools.models import ToolOutput
-
-from texttools import TheTool
-
-from backend.schemas import Config, ProcessRequest
+from backend.routers import router
+from fastapi import FastAPI
 
 app = FastAPI(
     title="TextTools App",
@@ -13,33 +8,9 @@ app = FastAPI(
     version="0.1.0",
 )
 
-
-tool = None
-
-
-@app.post("/configuration", status_code=status.HTTP_201_CREATED)
-def create_config(config: Config):
-    try:
-        client = OpenAI(api_key=config.api_key, base_url=config.base_url)
-        global tool
-        tool = TheTool(client=client, model=config.model)
-        return {"message": "Created TheTool successfully"}
-
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+app.include_router(router)
 
 
-@app.post("/process", status_code=status.HTTP_200_OK)
-def create_output(request: ProcessRequest) -> ToolOutput:
-    if not tool:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Set configs first"
-        )
-
-    try:
-        method = getattr(tool, request.operation)
-        output = method(request.text)
-        return output
-
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+@app.get("/")
+def root():
+    return {"message": "Welcome to TextTools App"}
