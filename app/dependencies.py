b910b90ac54d0732086c73cd68a5c097ed532b89
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-25 12:51:42 +0330

    fix: use AsyncOpenAI

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,10 @@
+from openai import AsyncOpenAI
+
 from texttools import AsyncTheTool
-from openai import OpenAI
 
 from .config import settings
 
 
 def get_tool() -> AsyncTheTool:
-    client = OpenAI(api_key=settings.api_key, base_url=settings.base_url)
+    client = AsyncOpenAI(api_key=settings.api_key, base_url=settings.base_url)
     return AsyncTheTool(client=client, model=settings.llm_model)
